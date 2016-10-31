c82d4c02453274746c3a1ccb6cf7f89f25f8541a
Eric Raetz and Suzi Rubino <eric@>
2016-10-31 12:19:00 -0700

    add place display functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-
-
-function LocationSet(place, season, lodging) {
+function Place(place) {
 	this.place = place;
+}
+
+function LocationSet(season, lodging) {
 	this.season = season;
 	this.lodging = lodging;
 }
 
+Place.prototype.fullPlace = function() {
+	return "<h3>" + this.place + "</h3>";
+}
+
 LocationSet.prototype.fullLocation = function() {
-	return "<li>" + this.place + "</li>" +  "<li>" + this.season + "</li>" + "<li>" + this.lodging + "</li>";
+	return "<li>" + this.season + "</li>" + "<li>" + this.lodging + "</li>";
 }
 
 function resetFields() {
@@ -24,9 +29,14 @@ $(document).ready(function() {
 		$("ul#locationInfo").text("");
 
 		var inputPlace = $("input#newPlace").val();
+		var inputPlaceFull = new Place(inputPlace);
+
 		var inputSeason = $("input#newSeason").val();
 		var inputLodging = $("input#newLodging").val();
-		var inputSet = new LocationSet(inputPlace, inputSeason, inputLodging);
+		var inputSet = new LocationSet(inputSeason, inputLodging);
+
+		$("div#placeDisplay").append(inputPlaceFull.fullPlace());
+
 
 		$("ul#locationInfo").append(inputSet.fullLocation());
 
